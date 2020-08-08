5249d60b98675b5a74f9e6e55489cd7a9576dbb0
Jerod Rhodes <63427467+jerodrhodes@users.noreply.github.com>
2020-08-08 14:35:57 -0500

    updated app.js to fix dropdown and plotly changes

--- Code/static/js/app.js
+++ Code/static/js/app.js
@@ -1,4 +1,4 @@
-function getData() {
+function buildPlots() {
     // Read in json data
     d3.json("samples.json").then(bellydata => {
         console.log(bellydata)
@@ -9,7 +9,7 @@ function getData() {
         var sampleValues = bellydata.samples[0].sample_values.slice(0,10).reverse();
         console.log(sampleValues)
         // Get labels for top 10 otu ids.
-        var otuLabels = bellydata.samples[0].otu_labels.slice(0,10);
+        var otuLabels = bellydata.samples[0].otu_labels.slice(0,10).reverse();
         console.log(`OTU labels: ${otuLabels}`)
         // get top 10 otu ids for the bar plot and reverse it
         var topOtuIds = bellydata.samples[0].otu_ids.slice(0,10).reverse();
@@ -30,9 +30,6 @@ function getData() {
         var data = [trace1];
         var layout = {
             title: "Top 10 OTUs",
-            // yaxis: {
-            //     tickmode: "linear"
-            // },
             margin: {
                 l: 100,
                 r: 100,
@@ -42,7 +39,7 @@ function getData() {
         };
 
         // Create bar plot
-        Plotly.newPlot("bar", data, layout);
+        Plotly.react("bar", data, layout);
 
         // Info for bubble plot
         var trace2 = {
@@ -59,20 +56,26 @@ function getData() {
         // Data variable
         var data1 = [trace2];
         var layout1 = {
+            title: "Patient OTU Counts",
             xaxis: {title: "OTU ID"},
             height: 600,
             width: 1200
         };
 
         // Create bubble plot
-        Plotly.newPlot("bubble", data1, layout1);
-        });
-    };
+        Plotly.react("bubble", data1, layout1);
 
-// Function for metadata
-function getInfo() {
-    // Read in json data
-    d3.json("samples.json").then((bellydata) =>{
+        // Dropdown
+        dropdown = document.getElementById('selDataset');
+        defaultOption = document.createElement('option')
+        options = bellydata.names
+        for (i=0; i < options.length; i++){
+            option = document.createElement('option');
+            option.text = options[i];
+            option.value = options[i];
+            dropdown.add(option);
+        };
+
         // Collect metadata for demo panel
         var metadata = bellydata.metadata;
         console.log(metadata)
@@ -92,34 +95,118 @@ function getInfo() {
         // Grab demo data for selected id and append to panel
         Object.entries(result).forEach((key) => {
             demographicInfo.append("h5").text(key[0].toUpperCase() + ": " + key[1] + "\n");
+            });
         });
-    });
-}   
+    };  
 
 // Function for change
-function optionChanged() {
-    getData();
-    getInfo();
-}
+d3.selectAll("#selDataset").on("change", getData);
 
-// Function for initial page layout
-function init() {
-    // choose dropdown menu
-    var dropdown = d3.select("#selDataset");
+function getData() {
 
-    // read data again
-    d3.json("samples.json").then((bellydata) => {
-        console.log(bellydata)
+    dropDown = d3.select("#selDataset");
+    idInfo = dropDown.property("value");
+    console.log(idInfo)
+    d3.json("samples.json").then(bellydata => {
+        idNo = bellydata.names
+        for (i = 0; i < idNo.length; i++) {
+            if (idInfo == idNo[i]) {
+                // Bar Plot
+                console.log("success")
+                var otuIds = bellydata.samples[i].otu_ids;
+                var sampleValues = bellydata.samples[i].sample_values.slice(0,10).reverse();
+                var otuLabels = bellydata.samples[i].otu_labels.slice(0,10).reverse();
+                var topOtuIds = bellydata.samples[i].otu_ids.slice(0,10).reverse();
+                var otuIdLabels = topOtuIds.map(d => "OTU " + d);
 
-        // pull id data to dropdown
-        bellydata.names.forEach(function(name) {
-            dropdown.append("option").text(name).property("value");
-        });
+                var trace1 = {
+                    x: sampleValues,
+                    y: otuIdLabels,
+                    text: otuLabels,
+                    marker: {
+                        color: 'green'
+                    },
+                    type: "bar",
+                    orientation: "h"
+                };
 
-        // Call functions to display data and render plots
-        getData(bellydata.names[0]);
-        getInfo(bellydata.names[0]);
+                var data1 = [trace1];
+                var layout = {
+                    title: "Top 10 OTUs",
+                    margin: {
+                        l: 100,
+                        r: 100,
+                        t: 100,
+                        b: 100
+                    }
+                };
+
+                // Bubble Chart
+                var trace2 = {
+                    x: bellydata.samples[i].otu_ids,
+                    y: bellydata.samples[i].sample_values,
+                    mode: "markers",
+                    marker:{
+                        size: bellydata.samples[i].sample_values,
+                        color: bellydata.samples[i].otu_ids
+                    },
+                    text: bellydata.samples[i].otu_labels
+                };
+
+                var data2 = [trace2];
+                var layout1 = {
+                    title: "Patient OTU Counts",
+                    xaxis: {title: "OTU ID"},
+                    height: 600,
+                    width: 1200
+                };
+
+                //Demographic Info
+                var metadata = bellydata.metadata;
+
+                console.log(metadata)
+                var result = metadata.filter(meta => meta.id === +idInfo)[0];
+        
+                var demographicInfo = d3.select("#sample-metadata");
+
+                demographicInfo.html("");
+                console.log(result)
+                Object.entries(result).forEach((key) => {
+                    demographicInfo.append("h5").text(key[0].toUpperCase() + ": " + key[1] + "\n");
+                });
+        updatePlotly(data1, data2, result)
+            }
+        }
     });
 }
 
-init();
+function updatePlotly(newdata1, newdata2, result) {
+    var layout1 = {
+        title: 'Top 10 OTUs',
+        xaxis: {
+            title: {
+              text: 'OTU IDs'}}
+        };
+    
+    var layout2 = {
+        height: 600,
+        width: 1200,
+        title: 'Patient OTU Counts',
+        xaxis: {
+            title: {
+              text: 'OTU IDs'}}
+        };
+    Plotly.react('bar',newdata1, layout1);
+    Plotly.react('bubble', newdata2, layout2);
+
+    box = d3.selectAll('#sample-metadata');
+    box.html('');
+    
+    Object.entries(result).forEach(([key,value]) => {
+        console.log(`${key}: ${value}`);
+        box.append('ul').text(`${key}: ${value}`);
+    });
+}
+
+getData();
+buildPlots();
